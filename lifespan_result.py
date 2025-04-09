520a30b11752eb92646e7566da07cc45d9b983bf
li-yan-study <204747899@qq.com>
2025-04-09 17:32:04 +0800

    Update lifespan_result.py

--- lifespan_result.py
+++ lifespan_result.py
@@ -1,5 +1,5 @@
 # 分男女运行： CUDA_VISIBLE_DEVICES=0 python test.py --name males_model --which_epoch latest --display_id 0 --traverse --interp_step 0.05 --image_path_file males_image_list.txt  --in_the_wild --verbose 
-
+# 处理lifespan的结果 变成子目录为年龄的格式
 
 import os
 import shutil
