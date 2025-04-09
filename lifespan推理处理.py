b77bfe141df712f51c91675321212e181f3d4bc2
li-yan-study <204747899@qq.com>
2025-04-09 17:28:16 +0800

    Create  lifespan推理处理.py

---
+++ lifespan推理处理.py
@@ -0,0 +1,20 @@
+# 让lifespan能够正常推理，不检测关键点
+# 找到data/multiclass_unaligned_dataset.py中的def get_item_from_path(self, path): 函数，修改 if self.in_the_wild:部分如下
+    def get_item_from_path(self, path):
+        path_dir, im_name = os.path.split(path)
+        img = Image.open(path).convert('RGB')
+        img = np.array(img.getdata(), dtype=np.uint8).reshape(img.size[1], img.size[0], 3)
+
+        if self.in_the_wild:
+            # img, parsing = self.preprocessor.forward(img)
+            parsing_path = os.path.join(path_dir, 'parsings', im_name[:-4] + '.jpg')
+            parsing = Image.open(parsing_path).convert('RGB')
+            parsing = np.array(parsing.getdata(), dtype=np.uint8).reshape(parsing.size[1], parsing.size[0], 3)
+        
+        else:
+            parsing_path = os.path.join(path_dir, 'parsings', im_name[:-4] + '.png')
+            parsing = Image.open(parsing_path).convert('RGB')
+            parsing = np.array(parsing.getdata(), dtype=np.uint8).reshape(parsing.size[1], parsing.size[0], 3)
+
+        img = Image.fromarray(self.mask_image(img, parsing))
+        img = self.transform(img).unsqueeze(0)
