0cf600a196051195675b55bb9b2fb2c0d2fa36e3
li-yan-study <204747899@qq.com>
2025-04-09 16:58:44 +0800

    Update lifespan_result.py

--- lifespan_result.py
+++ lifespan_result.py
@@ -1,4 +1,4 @@
-# 运行： CUDA_VISIBLE_DEVICES=0 python test.py --name males_model --which_epoch latest --display_id 0 --traverse --interp_step 0.05 --image_path_file males_image_list.txt  --in_the_wild --verbose 
+# 分男女运行： CUDA_VISIBLE_DEVICES=0 python test.py --name males_model --which_epoch latest --display_id 0 --traverse --interp_step 0.05 --image_path_file males_image_list.txt  --in_the_wild --verbose 
 
 
 import os
