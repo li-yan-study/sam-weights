d49b2f943cc24a58e236b0474259290f2347e1b5
li-yan-study <204747899@qq.com>
2025-04-09 16:58:25 +0800

    Update lifespan_result.py

--- lifespan_result.py
+++ lifespan_result.py
@@ -1,3 +1,6 @@
+# 运行： CUDA_VISIBLE_DEVICES=0 python test.py --name males_model --which_epoch latest --display_id 0 --traverse --interp_step 0.05 --image_path_file males_image_list.txt  --in_the_wild --verbose 
+
+
 import os
 import shutil
 import re
