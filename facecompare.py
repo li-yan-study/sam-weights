f81c20a043dfc22bb8e553f2295acfc051167eb9
li-yan-study <204747899@qq.com>
2025-04-09 16:14:39 +0800

    Update facecompare.py

--- facecompare.py
+++ facecompare.py
@@ -42,7 +42,7 @@ def compareIm(faceId1, faceId2, similarity_threshold=75):
     
     return None  # 表示请求失败或不满足条件
 
-def find_and_compare_images(dir1, dir2, similarity_threshold=75):
+def find_and_compare_images(dir1, dir2,out_file,similarity_threshold=75):
     """
     遍历两个目录，找到相同文件名的图片，并使用Face++ API比较它们的相似度。
     
@@ -52,29 +52,33 @@ def find_and_compare_images(dir1, dir2, similarity_threshold=75):
 
     - similarity_threshold: 相似度阈值，默认为75。
     """
-    # 获取两个目录下的文件列表
-    files_dir1 = set(os.listdir(dir1))
-    files_dir2 = set(os.listdir(dir2))
-    
-    # 找到两个目录中共有的文件名
-    common_files = files_dir1.intersection(files_dir2)
-    
-    for filename in common_files:
-        # 构建完整的文件路径
-        path1 = os.path.join(dir1, filename)
-        path2 = os.path.join(dir2, filename)
-        
-        # 调用compareIm函数进行比较
-        confidence = compareIm(path1, path2, similarity_threshold)
-        
-        if confidence is not None:
-            print(f"文件 {filename} 的相似度为：{confidence}")
-        else:
-            print(f"文件 {filename} 无法比较或未达到阈值。")
-        
-    print("所有相同文件名的图片已处理完成。")
+    with open(out_file, "a") as f:
+        # 获取两个目录下的文件列表
+        files_dir1 = set(os.listdir(dir1))
+        files_dir2 = set(os.listdir(dir2))
+        target_age = os.path.basename(dir2)
+        # 找到两个目录中共有的文件名
+        common_files = files_dir1.intersection(files_dir2)
+        confidences = []
+        for filename in common_files:
+            # 构建完整的文件路径
+            path1 = os.path.join(dir1, filename)
+            path2 = os.path.join(dir2, filename)
+            
+            # 调用compareIm函数进行比较
+            confidence = compareIm(path1, path2, similarity_threshold)
+            confidences.append(compareIm)
+            if confidence is not None:
+                print(f"文件 {filename} 的相似度为：{confidence}")
+            else:
+                print(f"文件 {filename} 无法比较或未达到阈值。")
+        average_confidence = sum(confidences) / len(confidences)
+        avg_str= f"目标年龄：{target_age},对比差异：{average_confidence}"
+        f.write(avg_str + "\n")
+        print("所有相同文件名的图片已处理完成。")
 
 # 使用示例
-dir_path1 = "path/to/your/directory1"
-dir_path2 = "path/to/your/directory2"
-find_and_compare_images(dir_path1, dir_path2)
+dir_path1 = "path/to/your/directory1"  # 放原图路径
+dir_path2 = "path/to/your/directory2"  # 放目标年龄目录路径
+out_file = "XX_face_compare.txt"
+find_and_compare_images(dir_path1, dir_path2,out_file=)
